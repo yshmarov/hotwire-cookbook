08b5f9bb9260320c03b7fd01b7e736a242e59eef
yshmarov <yshmarov@gmail.com>
2023-10-01 14:06:36 +0300

    staic controller, dashboard action

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+  def dashboard
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  root to: redirect('/employees')
+  get 'dashboard', to: 'static#dashboard'
   resources :employees
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
